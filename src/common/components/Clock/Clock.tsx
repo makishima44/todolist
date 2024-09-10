f2250ad4556ab56204d63f6d900e47a4ae17999f
Artur <metallist15431543@gmail.com>
2024-09-10 21:08:26 +0300

    Стилизовал компоненту Clock

--- src/common/components/Clock/Clock.tsx
+++ src/common/components/Clock/Clock.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import s from "./Clock.module.css";
 
 const Clock = () => {
   const [time, setTime] = useState(new Date());
@@ -11,13 +12,44 @@ const Clock = () => {
     return () => clearInterval(interval);
   }, []);
 
+  const daysOfWeek = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+  ];
+  const months = [
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря",
+  ];
+
+  const dayOfWeek = daysOfWeek[time.getDay()];
+  const dayOfMonth = time.getDate();
+  const month = months[time.getMonth()];
+
   const hours = time.getHours().toString().padStart(2, "0");
   const minutes = time.getMinutes().toString().padStart(2, "0");
   const seconds = time.getSeconds().toString().padStart(2, "0");
+
   const timeString = `${hours}:${minutes}:${seconds}`;
+  const dateString = `${dayOfWeek} ${dayOfMonth} ${month}`;
   return (
-    <div>
-      <h2>{timeString}</h2>
+    <div className={s.clockContainer}>
+      <p className={s.dateText}>{dateString}</p>
+      <p className={s.timeText}>{timeString}</p>
     </div>
   );
 };
